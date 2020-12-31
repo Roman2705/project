7878a41348e79f125cfb3b47b2c0b4f6c0e92bdc
Roman <ragerasimchuk@gmail.com>
2020-12-31 11:46:09 +0200

    second

---
+++ script.js
@@ -0,0 +1,6 @@
+"usestrict";
+//alert("Привет");
+//onst result = confirm ("Чи хочете ви пити?");
+//console.log(result);
+const answer = prompt("Are you have 18 y.o.?", "");
+console.log(typeof(answer));
